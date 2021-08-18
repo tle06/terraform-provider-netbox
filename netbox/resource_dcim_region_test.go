0e7cad569a1abc6e9523064e4fa5f96831e5abd2
tle06 <tle@tlnk.fr>
2021-08-18 11:42:39 +0200

    add test

---
+++ netbox/resource_dcim_region_test.go
@@ -0,0 +1,108 @@
+package netbox
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/go-openapi/runtime"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/netbox-community/go-netbox/netbox/client/dcim"
+)
+
+func TestAccDcimRegion_basic(t *testing.T) {
+	name := "test region"
+	slug := "test-region"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDcimRegionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDcimRegionConfigBasic(name, slug),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDcimRegionExists("netbox_dcim_region.test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDcimRegionDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*client.NetBoxAPI)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "netbox_dcim_region" {
+			continue
+		}
+
+		prefixID, err := strconv.ParseInt(rs.Primary.ID, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		params := &dcim.DcimRegionsReadParams{
+			Context: context.Background(),
+			ID:      prefixID,
+		}
+
+		resp, err := c.Dcim.DcimRegionsRead(params, nil)
+		if err != nil {
+			if err.(*runtime.APIError).Code == 404 {
+				return nil
+			}
+
+			return err
+		}
+
+		return fmt.Errorf("Region ID still exists: %d", resp.Payload.ID)
+	}
+
+	return nil
+}
+
+func testAccCheckDcimRegionExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No region ID set")
+		}
+
+		c := testAccProvider.Meta().(*client.NetBoxAPI)
+
+		regionID, err := strconv.ParseInt(rs.Primary.ID, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		params := &dcim.DcimRegionsReadParams{
+			Context: context.Background(),
+			ID:      regionID,
+		}
+
+		_, err = c.Dcim.DcimRegionsRead(params, nil)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckDcimRegionConfigBasic(name string, slug string) string {
+	return fmt.Sprintf(`
+	resource "netbox_dcim_region" "example" {
+		name = "%s"
+		slug = "%s"
+	  }
+`, name, slug)
+}
